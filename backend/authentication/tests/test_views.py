bfcd9dafcc0f1873ca630a99be16d71d37707415
UFEddy <eddy.rosales@ufl.edu>
2023-11-11 23:27:32 -0500

    Adding in views test cases for auth app

---
+++ backend/authentication/tests/test_views.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+
+class AuthTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.register_url = reverse('register')
+        self.login_url = reverse('login')
+        self.user_data = {
+            'username': 'testuser',
+            'email': 'test@example.com',
+            'password': 'testpass123'
+        }
+
+    def test_user_registration(self):
+        response = self.client.post(self.register_url, self.user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+    def test_user_login(self):
+        self.client.post(self.register_url, self.user_data, format='json')
+        response = self.client.post(self.login_url, self.user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
