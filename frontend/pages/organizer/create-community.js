bbe51f949aa898e38793cb1f8d79d514025fb1bc
kurtweber12 <kkw1995@hotmail.com>
2023-11-27 14:51:20 -0500

    layout for comminities list page for organizer

---
+++ frontend/pages/organizer/create-community.js
@@ -0,0 +1,12 @@
+import Header from "@/components/Header";
+import React from "react";
+
+const CreateCommunity = () => {
+	return (
+		<main>
+			<Header />
+		</main>
+	);
+};
+
+export default CreateCommunity;
